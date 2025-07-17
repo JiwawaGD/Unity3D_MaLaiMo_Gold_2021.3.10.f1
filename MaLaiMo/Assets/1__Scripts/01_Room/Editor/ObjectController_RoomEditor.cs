51936b383b0c66948da3289ce0fb5c76205dc20b
Lerbo <qaz431220@gmail.com>
2025-07-18 02:33:10 +0800

    室內優化 : 新增腳本做出可折疊的區塊

---
+++ MaLaiMo/Assets/1__Scripts/01_Room/Editor/ObjectController_RoomEditor.cs
@@ -0,0 +1,75 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ObjectController_Room))]
+[CanEditMultipleObjects]
+public class ObjectController_RoomEditor : Editor
+{
+    private bool showGameObjects = true;
+    private bool showItems = true;
+
+    // GameObject 欄位
+    SerializedProperty player;
+    SerializedProperty wallClock;
+
+    // ItemController 欄位
+    SerializedProperty grandmaRoomDoor;
+    SerializedProperty lotusPaper;
+    SerializedProperty foldedLotusPaper;
+    SerializedProperty offeringPlace;
+    SerializedProperty grandmaRoomCloset;
+    SerializedProperty clothesInCloset;
+    SerializedProperty graffitiInCloset;
+
+    private void OnEnable()
+    {
+        // GameObject 欄位
+        player = serializedObject.FindProperty("_player");
+        wallClock = serializedObject.FindProperty("_wallClock");
+
+        // ItemController 欄位
+        grandmaRoomDoor = serializedObject.FindProperty("_grandmaRoomDoor");
+        lotusPaper = serializedObject.FindProperty("_lotusPaper");
+        foldedLotusPaper = serializedObject.FindProperty("_foldedLotusPaper");
+        offeringPlace = serializedObject.FindProperty("_offeringPlace");
+        grandmaRoomCloset = serializedObject.FindProperty("_grandmaRoomCloset");
+        clothesInCloset = serializedObject.FindProperty("_clothesInCloset");
+        graffitiInCloset = serializedObject.FindProperty("_graffitiInCloset");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("==== 場景物件分組 ====", EditorStyles.boldLabel);
+
+        showGameObjects = EditorGUILayout.Foldout(showGameObjects, "Game Object 欄位");
+        if (showGameObjects)
+        {
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(player, new GUIContent("玩家"));
+            EditorGUILayout.PropertyField(wallClock, new GUIContent("壁鐘"));
+            EditorGUI.indentLevel--;
+        }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("==== 互動物件分組 ====", EditorStyles.boldLabel);
+
+        showItems = EditorGUILayout.Foldout(showItems, "Item Controller 欄位");
+        if (showItems)
+        {
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(grandmaRoomDoor, new GUIContent("奶奶房間門"));
+            EditorGUILayout.PropertyField(lotusPaper, new GUIContent("蓮花紙張"));
+            EditorGUILayout.PropertyField(foldedLotusPaper, new GUIContent("摺完的蓮花"));
+            EditorGUILayout.PropertyField(offeringPlace, new GUIContent("放拜飯的地方"));
+            EditorGUILayout.PropertyField(grandmaRoomCloset, new GUIContent("奶奶房間衣櫃"));
+            EditorGUILayout.PropertyField(clothesInCloset, new GUIContent("衣櫃裡的衣服"));
+            EditorGUILayout.PropertyField(graffitiInCloset, new GUIContent("衣櫃裡的塗鴉畫"));
+            EditorGUI.indentLevel--;
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
--- MaLaiMo/Assets/1__Scripts/01_Room/ObjectController_Room.cs
+++ MaLaiMo/Assets/1__Scripts/01_Room/ObjectController_Room.cs
@@ -2,8 +2,34 @@
 
 public class ObjectController_Room : MonoBehaviour
 {
-    [Header("壁鐘")]
+    #region ============ 物件 相關 ============
+    [HideInInspector]
+    public GameObject _player;
+
+    [HideInInspector]
     public GameObject _wallClock;
+    #endregion
 
+    #region ============ Item 相關 ============
+    [HideInInspector]
+    public ItemController _grandmaRoomDoor;
 
+    [HideInInspector]
+    public ItemController _lotusPaper;
+
+    [HideInInspector]
+    public ItemController _foldedLotusPaper;
+
+    [HideInInspector]
+    public ItemController _offeringPlace;
+
+    [HideInInspector]
+    public ItemController _grandmaRoomCloset;
+
+    [HideInInspector]
+    public ItemController _clothesInCloset;
+
+    [HideInInspector]
+    public ItemController _graffitiInCloset;
+    #endregion
 }

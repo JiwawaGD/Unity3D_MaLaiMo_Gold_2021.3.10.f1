f2f2c12b7d8c5828f2954240582863fafba4a3b8
tony <jeff19971006@gmail.com>
2025-08-20 23:09:56 +0800

    森林場景:鬼魂凝視兩秒後追逐、鬼魂animator設置

--- MaLaiMo/Assets/1__Scripts/03_Forest/Ghost.cs
+++ MaLaiMo/Assets/1__Scripts/03_Forest/Ghost.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
 
 public class Ghost : MonoBehaviour
 {
     public Transform Player;
+    public Animator GhostAnimator;
     private float maxDistance = 15f;
-
-
+    private bool alreadyNoticed = false;
+    private bool alreadyCallNoticedFuc = false;
     void FixedUpdate()
     {
         float distance = Vector3.Distance(transform.position, Player.position);
         if (distance < maxDistance)
         {
             maxDistance = 1000f;
-            GetComponent<NavMeshAgent>().SetDestination(Player.position);
+            if (alreadyNoticed == false && alreadyCallNoticedFuc == false) StartCoroutine(NoticedPlayer());
+            else if (alreadyNoticed == true) GetComponent<NavMeshAgent>().SetDestination(Player.position);
         }
+    }
+    IEnumerator NoticedPlayer()
+    {
+        alreadyCallNoticedFuc = true;
+        yield return new WaitForSeconds(2f);
+        GhostAnimator.SetTrigger("hunt");
+        alreadyNoticed = true;
     }
 }

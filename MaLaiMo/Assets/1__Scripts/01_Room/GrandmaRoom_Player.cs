f4dde9611b9f789d4a35f5033b3eb82e30353f4e
tony2020-god <tonystark1006@gmail.com>
2026-01-11 22:31:32 +0800

    回憶: 4-3鬼腳本

--- MaLaiMo/Assets/1__Scripts/01_Room/GrandmaRoom_Player.cs
+++ MaLaiMo/Assets/1__Scripts/01_Room/GrandmaRoom_Player.cs
@@ -5,36 +5,4 @@ using UnityEngine;
 
 public class GrandmaRoom_Player : PlayerController
 {
-    public string eyeStates = "";
-    public Transform[] Eyes;
-    public override void Update()
-    {
-        base.Update();
-        if(eyeStates != "")
-        {
-            if (Input.GetKeyDown(KeyCode.Alpha1)) openEyes();
-            else if (Input.GetKeyDown(KeyCode.Alpha2)) helfOpenEyes();
-            else if (Input.GetKeyDown(KeyCode.Alpha3)) closeEyes();
-        }
-        if(eyeStates == "open") tfPlayerCamera.position += Vector3.right * 0.05f * Time.deltaTime;
-    }
-
-    private void openEyes()
-    {
-        eyeStates = "open";
-        Eyes[0].DOLocalMoveY(1050f, 0.5f);
-        Eyes[1].DOLocalMoveY(-1050f, 0.5f);
-    }
-    private void helfOpenEyes()
-    {
-        eyeStates = "helfOpen";
-        Eyes[0].DOLocalMoveY(650f, 0.5f);
-        Eyes[1].DOLocalMoveY(-650f, 0.5f);
-    }
-    private void closeEyes()
-    {
-        eyeStates = "close";
-        Eyes[0].DOLocalMoveY(250f, 0.5f);
-        Eyes[1].DOLocalMoveY(-250f, 0.5f);
-    }
 }
---
+++ MaLaiMo/Assets/1__Scripts/04_Memory/GrandmaRoom_Memory_Player.cs
@@ -0,0 +1,43 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrandmaRoom_Memory_Player : PlayerController
+{
+    public bool dead = false;
+    public string eyeStates = "";
+    public Transform[] Eyes;
+
+    public override void Update()
+    {
+        base.Update();
+        if (dead) return;
+        if(eyeStates != "")
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1)) openEyes();
+            else if (Input.GetKeyDown(KeyCode.Alpha2)) helfOpenEyes();
+            else if (Input.GetKeyDown(KeyCode.Alpha3)) closeEyes();
+        }
+        if(eyeStates == "open") tfPlayerCamera.position += Vector3.right * 0.05f * Time.deltaTime;
+    }
+
+    public void openEyes()
+    {
+        eyeStates = "open";
+        Eyes[0].DOLocalMoveY(1050f, 0.5f);
+        Eyes[1].DOLocalMoveY(-1050f, 0.5f);
+    }
+    private void helfOpenEyes()
+    {
+        eyeStates = "helfOpen";
+        Eyes[0].DOLocalMoveY(650f, 0.5f);
+        Eyes[1].DOLocalMoveY(-650f, 0.5f);
+    }
+    private void closeEyes()
+    {
+        eyeStates = "close";
+        Eyes[0].DOLocalMoveY(250f, 0.5f);
+        Eyes[1].DOLocalMoveY(-250f, 0.5f);
+    }
+}
